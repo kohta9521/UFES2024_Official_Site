618b6d69b3aca1dfaf478aaacba3a07c01ced486
k_95.21 <kohta0421kochi@gmail.com>
2024-02-10 10:25:09 +0900

    add first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/page.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.main}>
+      <h1>hello this is new wave fist event site</h1>
+    </div>
+  );
+}
