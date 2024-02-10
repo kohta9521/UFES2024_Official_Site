643e9562a8092cefda0c43704c4f8c74236b2db1
k_95.21 <kohta0421kochi@gmail.com>
2024-02-10 10:57:49 +0900

    ローディングアニメーションの追加と画面遷移のロジックの追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,26 @@
+"use client";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/page.module.scss";
 
+// components import
+import Loading from "../components/templates/Loading";
+
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // ローディングが完了したらisLoadingをfalseに設定
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000); // 1秒後にローディングを非表示にする
+
+    return () => clearTimeout(timer); // コンポーネントのアンマウント時にタイマーをクリア
+  }, []);
+
+  // ローディング中はLoadingコンポーネントを表示
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <div className={styles.main}>
       <h1>hello this is new wave fist event site</h1>
---
+++ components/templates/stories/Loading.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Loading from "../Loading";
+
+const meta: Meta = {
+  title: "Components/Loading",
+  component: Loading,
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof Loading> = {};
