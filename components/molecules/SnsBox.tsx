fe53085c3a4bfdd97881fe28c254ab920b99d445
k_95.21 <kohta0421kochi@gmail.com>
2024-02-13 23:32:03 +0900

    add new library about react icons

---
+++ components/molecules/SnsBox.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// scss import
+import styles from './styles/SnsBox.module.scss';
+
+const SnsBox = () => {
+    return (
+        <div className={styles.box}>
+            <span className={styles.verticelBorder}></span>
+            
+        </div>
+    )
+}
--- components/molecules/TopListBox.tsx
+++ components/molecules/TopListBox.tsx
@@ -6,7 +6,11 @@ import styles from "./styles/TopListBox.module.scss";
 // components import
 
 const TopListBox = () => {
-  return <div className={styles.topListBox}></div>;
+  return <div className={styles.topListBox}>
+    <div className={styles.container}>
+      
+    </div>
+  </div>;
 };
 
 export default TopListBox;
