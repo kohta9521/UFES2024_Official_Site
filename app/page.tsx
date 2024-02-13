43f3aab36817f7f0ecdcd3cf80da0a8457908394
k_95.21 <kohta0421kochi@gmail.com>
2024-02-13 22:47:44 +0900

    add new bg image molecules components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 import React, { useState, useEffect } from "react";
+
+// scss import
 import styles from "../styles/page.module.scss";
 
 // components import
 import Loading from "../components/templates/Loading";
 import Top from "@/components/organisms/Top";
+import About from "@/components/organisms/About";
+import BgImage from "@/components/organisms/BgImage";
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
@@ -23,7 +27,9 @@ export default function Home() {
   }
   return (
     <div className={styles.main}>
+      <BgImage />
       <Top />
+      <About />
     </div>
   );
 }
---
+++ components/organisms/BgImage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// scss import
+import styles from "./styles/BgImage.module.scss";
+
+const BgImage = () => {
+  return <div className={styles.bgImage}></div>;
+};
+
+export default BgImage;
