d5709b871f0e0eb51839a56844b10e7b669b0d0f
k_95.21 <kohta0421kochi@gmail.com>
2024-02-15 19:21:39 +0900

    add responsive hamburger menu

--- components/organisms/Top.tsx
+++ components/organisms/Top.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import React from "react";
 import Image from "next/image";
 
@@ -9,10 +11,17 @@ import TopVerticalText from "../atoms/TopVerticalText";
 import ScrollDownText from "../atoms/ScrollDownText";
 import TopListBox from "../molecules/TopListBox";
 import SnsBox from "../molecules/SnsBox";
+import HamburgerBtn from "../atoms/HamburgerBtn";
 
 const Top = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <div className={styles.top}>
+      <HamburgerBtn isOpen={false} toggle={toggle} />
       <TopVerticalText
         id={1}
         text="春の大学生イベントを一つのカルチャーに"
