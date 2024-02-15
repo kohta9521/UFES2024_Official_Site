285a9bc9f39e6e16fc2ed94296e1f2789bf82c8f
k_95.21 <kohta0421kochi@gmail.com>
2024-02-15 19:28:43 +0900

    aed useaos animation

--- components/organisms/About.tsx
+++ components/organisms/About.tsx
@@ -4,7 +4,11 @@ import styles from "./styles/About.module.scss";
 // components import
 import SectionTitle from "../atoms/SectionTitle";
 
+// hooks import
+import useAOS from "@/hooks/useAOS";
+
 const About = () => {
+  useAOS();
   return (
     <div className={styles.about}>
       <div className={styles.container}>
@@ -16,8 +20,10 @@ const About = () => {
           align="left"
         />
         <div className={styles.mainTextBox}>
-          <p className={styles.titleText}>UFESとは</p>
-          <p className={styles.mainText}>
+          <p className={styles.titleText} data-aos="fade-up">
+            UFESとは
+          </p>
+          <p className={styles.mainText} data-aos="fade-up">
             UFESは「春の大学生フェス」を一つのカルチャーにすべく立ち上がった学生団体の合同新歓イベントです。
             <br />
             大学に属していない学生団体は各々がイベントを行っていました。様々なカラーの学生団体が集い、ともに一つのイベントを作り上げることで学生団体同士の新たな化学反応と横のつながりを目指します。
@@ -25,7 +31,7 @@ const About = () => {
             大学生活は有限です。所属するコミュニティによって学生生活はガラリと異なることでしょう。新入生には様々な学生団体を同じ場で比較することで、より自分とマッチする学生団体と出会い有意義な学生生活を送れる場を提供いたします。
             学生同士が新たな学生を想い生まれたイベントです。だからこそ主催側は思い入れと熱意があり、また参加者はカジュアルに楽しんでもらえるでしょう。企業考案の学生を入れたイベントとは全く異なること間違いなしです。
           </p>
-          <p className={styles.enMainText}>
+          <p className={styles.enMainText} data-aos="fade-up">
             UFES is a joint welcome event for student groups that was set up to
             make the Spring University Student Fest a part of the culture.
             Student groups that are not affiliated with a university have been
@@ -43,8 +49,8 @@ const About = () => {
             corporate event that includes students.
           </p>
         </div>
-        <span className={styles.underBorder}></span>
-        <div className={styles.underBox}>
+        <span className={styles.underBorder} data-aos="fade-up"></span>
+        <div className={styles.underBox} data-aos="fade-up">
           <div className={styles.leftBox}>
             <h3 className={styles.mainName}>Governing Body</h3>
             <p className={styles.name}>UFES 2024 実行委員会</p>
--- components/organisms/Top.tsx
+++ components/organisms/Top.tsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import React from "react";
 import Image from "next/image";
 
+// hooks import
+import useAOS from "@/hooks/useAOS";
+
 // scss import
 import styles from "./styles/Top.module.scss";
 
@@ -14,6 +17,7 @@ import SnsBox from "../molecules/SnsBox";
 import HamburgerBtn from "../atoms/HamburgerBtn";
 
 const Top = () => {
+  useAOS();
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => {
@@ -29,6 +33,7 @@ const Top = () => {
       />
       <div className={styles.centerTitleImage}>
         <Image
+          data-aos="fade-up"
           className={styles.center}
           src="/images/bg_white_logo.png"
           alt="centerTitleImage"
