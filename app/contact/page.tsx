93b05fd0d50c1c877950009514db5d696572c216
k_95.21 <kohta0421kochi@gmail.com>
2024-02-29 19:46:56 +0900

    fix vercel deploy error

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,7 +7,6 @@ import styles from "./Contact.module.scss";
 // components import
 import SubPageLayout from "@/components/templates/SubPageLayout";
 import SubMainContentLayout from "@/components/templates/SubMainContentLayout";
-import BgImage from "@/components/organisms/BgImage";
 
 const Contact = () => {
   // formの切り替え
